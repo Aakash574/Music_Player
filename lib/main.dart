099ae205cc0eb5acedbb0ba6ae1e0c31cae57ef1
Aakash574 <panwara925@gmail.com>
2023-08-14 00:32:47 +0530

    Music Player

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:musicplayer/src/presentation/view/list_of_music.dart';
+import 'package:musicplayer/src/utils/constant/Containers/gradient.dart';
+
+void main() {
+  Paint.enableDithering = true;
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(24),
+        decoration: const BoxDecoration(
+          gradient: customGradient,
+        ),
+        child: const Center(
+          child: ListOfMusicPlayer(),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/src/utils/constant/Containers/gradient.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+const customGradient = LinearGradient(
+  begin: Alignment.bottomCenter,
+  end: Alignment.topCenter,
+  colors: [
+    Color(0xFF1A1B1F),
+    Color(0xFF2E343A),
+  ],
+);
+const customGradient45 = LinearGradient(
+  begin: Alignment.bottomRight,
+  end: Alignment.topLeft,
+  colors: [
+    Color(0xFF1A1B1F),
+    Color(0xFF2E343A),
+  ],
+);
